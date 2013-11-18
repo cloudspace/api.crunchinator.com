0d85f90aec1cfdaca7ffeaaf3368eec0f83fa407
Kalon Hinds <kalon@cloudspace.com>
2013-11-18 15:39:50 -0500

    handle webservice keys:   * update README.md with instructions on how to ignore local changes to confg/environment_variables.rb   * add code to load local environment variables upon application start

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Load custom environment variables
+environment_variables = File.join(Rails.root, 'config', 'environment_variables.rb')
+load(environment_variables) if File.exists?(environment_variables)
+
 # Initialize the Rails application.
 Crunchinator::Application.initialize!
