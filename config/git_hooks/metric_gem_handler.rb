300c3ea77750fbe62bebca31b3797e71486f0ee9
Scott <scott@cloudspace.com>
2013-12-10 15:15:37 -0500

    Creating hook platform so we can add more code metric gems later

---
+++ config/git_hooks/metric_gem_handler.rb
@@ -0,0 +1,78 @@
+# Handles execution and printing of code metric gems specified in the exec method
+# Literally it just takes a command and prints results and returns false if it fails
+#
+# It will only exit with a status of 1 if you are on a major branch (master, development, staging)
+# And you are pushing.
+# 
+# If you are just commiting it won't keep you from committing but it will print the errors
+#
+# @!attribute [] git_hook_type
+#   @return [Integer] which type of git hook this class is being used on [:commit or :push]
+#
+# @!attribute [] git_branch
+#   @return [String] The current git branch the user is on
+#
+# @!attribute important_branches
+#   @return [Array] List of important branches that we should not allow a push if there are errors
+#
+# Something doesn't feel right about this class but hey its alpha so we'll probably need to re-write it
+class MetricGemHandler
+  #--------------------------------
+  # Constants
+  #--------------------------------
+  
+  GIT_HOOKS = {
+    :commit => 1,
+    :push => 2
+  }
+  
+  #--------------------------------
+  # Methods
+  #--------------------------------
+  
+  #
+  # Initializes the class with default variables
+  #
+  # @param git_hook_type [Symbol] What type of hook we are creating this class for :commit or :push
+  #
+  # @return nil
+  def initialize(git_hook_type = :commit)
+    @git_hook_type = GIT_HOOKS[git_hook_type.to_sym] || (raise "Invalid Git Hook Type")
+    @git_branch = `git branch | sed -e '/^[^*]/d' -e 's/* \\(.*\\)/\\1/'`.strip!
+    # @git_branch = "development"
+    @important_branches=%w(staging production development)
+  end
+
+  
+  
+  # 
+  # Runs the command and then prints the output if it fails
+  #
+  # @param cmd [String] The command to be processed
+  #
+  # @return [Boolean]
+  def exec(cmd)
+    results = `#{cmd}`
+    return print_and_fail($?,results)
+  end
+  
+  
+  private 
+  
+  #
+  # Prints any errors and returns false if it shouldn't execute the git command
+  #
+  # @param result [$?] The process that ran the script
+  # @param text [String] String returned from the script that ran
+  #
+  # @return [Boolean]
+  def print_and_fail(result,text)
+    if(result.exitstatus != 0)  
+      print text
+      if(@git_hook_type == GIT_HOOKS[:push] && @important_branches.include?(@git_branch))
+        return false
+      end
+    end
+    return true
+  end
+end
