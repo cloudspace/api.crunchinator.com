659523581f3acd7ca77f7dbc0b5684deac11189b
Jeremiah <jeremiah@cloudspace.com>
2014-01-27 14:00:34 +0000

    Configured a few more rails 4 features.

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require 'rails/all'
 Bundler.require(:default, Rails.env)
 
 module Crunchinator
+  # The main application object
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -19,5 +20,6 @@ module Crunchinator
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
   end
 end
