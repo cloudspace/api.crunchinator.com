d8e6d53690f69c2bdcf9077874856b37e58ed258
Justin Ridgewell <jridgewell@cloudspace.com>
2014-02-04 15:17:43 -0500

    Add a :valid_company Factory

--- spec/factories/company.rb
+++ spec/factories/company.rb
@@ -5,6 +5,14 @@ FactoryGirl.define do
     permalink { FactoryGirl.generate(:unique_company_name) }
     name { FactoryGirl.generate(:unique_company_name) }
     founded_on { 1.day.ago }
-    category
+
+    factory :valid_company do
+      category
+      after(:create) do |company|
+        create :headquarters, tenant: company
+        funding_round = create :funding_round, company: company
+        create :investment, funding_round: funding_round
+      end
+    end
   end
 end
