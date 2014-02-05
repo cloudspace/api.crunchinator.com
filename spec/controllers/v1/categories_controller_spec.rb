4ce8769bfeb7c699d514631b2e59a09de2dab5bd
Justin Ridgewell <jridgewell@cloudspace.com>
2014-02-05 14:43:51 -0500

    Break up CategoriesController specs

--- spec/controllers/v1/categories_controller_spec.rb
+++ spec/controllers/v1/categories_controller_spec.rb
@@ -9,10 +9,7 @@ describe V1::CategoriesController do
 
     describe 'appropriate response' do
       before(:each) do
-        @company = FactoryGirl.create(:valid_company)
-        @category = @company.category
-        @investor = FactoryGirl.create(:investor)
-        @investor.investments.first.funding_round.update_attribute :company, @company
+        @category = FactoryGirl.create(:valid_category)
       end
 
       it 'includes categories that have a valid company' do
@@ -21,17 +18,35 @@ describe V1::CategoriesController do
         category = JSON.parse(response.body)['categories'].first
         expect(category['id']).to eq(@category.id)
         expect(category['name']).to eq(@category.name)
-        expect(category['company_ids']).to eq([@company.id])
+      end
+
+      it 'includes company ids associated with the category' do
+        company_ids = @category.companies.pluck(:id)
+
+        get :index
+
+        category = JSON.parse(response.body)['categories'].first
+        expect(category['company_ids']).to eq(company_ids)
+      end
+
+      it 'includes investor ids associated with the category' do
+        company = @category.companies.first
+        @investor = FactoryGirl.create(:investor)
+        @investor.investments.first.funding_round.update_attribute :company, company
+
+        get :index
+
+        category = JSON.parse(response.body)['categories'].first
         expect(category['investor_ids']).to eq([@investor.guid])
       end
 
       it 'excludes categories that have no valid companies' do
-        invalid_category = FactoryGirl.create(:category)
+        excluded_category = FactoryGirl.create(:category, companies: [])
         get :index
 
         categories = JSON.parse(response.body)['categories']
         expect(categories.length).to eq(1)
-        expect(categories.map{ |c| c['id'] }).not_to include(invalid_category.id)
+        expect(categories.map{ |c| c['id'] }).not_to include(excluded_category.id)
       end
     end
   end
