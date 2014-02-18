b30561ae3ec9f2195e8871d4f137d7d572dcaf4b
Josh Lauer <jlauer@cloudspace.com>
2014-02-18 09:53:07 -0500

    fleshed out a comment

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,6 @@
 module Crunchinator
-  # yay linter
+  # The purpose of this is to store the name of the last git tab as a
+  # constant for use during the process of pushing json to S3
   class Application
     VERSION = `git tag`.split.last
   end
