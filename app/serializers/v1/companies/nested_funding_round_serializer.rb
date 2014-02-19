9fa9891e14f579c0e3c6041ff457bb26067608a3
Justin Ridgewell <justin@ridgewell.name>
2014-02-19 14:59:48 -0500

    Refactor NestedFundingRoundSerializer

--- app/serializers/v1/companies/nested_funding_round_serializer.rb
+++ app/serializers/v1/companies/nested_funding_round_serializer.rb
@@ -3,14 +3,10 @@ class V1::Companies::NestedFundingRoundSerializer < ActiveModel::Serializer
   attributes :id, :raised_amount, :funded_on, :investor_ids
 
   def funded_on
-    if @object.funded_on.present?
-      @object.funded_on.strftime('%-m/%-d/%Y')
-    else
-      nil
-    end
+    @object.funded_on.try(:strftime, '%-m/%-d/%Y')
   end
 
   def investor_ids
-    @object.investments.map { |investment| investment.investor_guid }
+    @object.investments.map(&:investor_guid)
   end
 end
