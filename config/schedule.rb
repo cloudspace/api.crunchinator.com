8efae588f0d7a3b2735d09d76ba00c58e9cab4fb
Scott <scott@cloudspace.com>
2014-02-19 16:43:34 -0500

    making sure the env var is passed to the schedule file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,37 @@
+
+
+
+# The api_queue run that hits the crunchbase api could take up to 3 days to run
+#
+# As a failsafe we are setting the staging cron to run every wednesday and to
+# use the archived data instead of hitting the crunchbase api
+
+
+set :output, "log/cron.log"
+
+# On production pull the data from the crunchbase api, archive it, and cache it
+# every :sunday, :at => '12am' do
+#   rake "api_queue:run", :environment => 'production'
+# end
+#
+# On staging read the data from the archive and cache it
+
+# @rails_env gets passed from capistrano
+if @rails_env == 'staging'
+  every :sunday, :at => '12am' do
+    rake "api_queue:run"
+  end
+else   
+  every :wednesday, :at => '12am' do
+    rake "api_queue:run[20,s3]"
+  end
+end
+
+
+# 
+
+
+
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
@@ -21,31 +55,3 @@
 # job_type :rake_with_full_path, "cd :path && RAILS_ENV=:environment /usr/local/bin/bundle exec /usr/local/bin/rake :task --silent :output"
 
 # Learn more: http://github.com/javan/whenever
-
-
-
-# The api_queue run that hits the crunchbase api could take up to 3 days to run
-#
-# As a failsafe we are setting the staging cron to run every wednesday and to
-# use the archived data instead of hitting the crunchbase api
-
-
-set :output, "log/cron.log"
-
-# On production pull the data from the crunchbase api, archive it, and cache it
-# every :sunday, :at => '12am' do
-#   rake "api_queue:run", :environment => 'production'
-# end
-
-if @rails_env == 'production'
-  every :sunday, :at => '12am' do
-    rake "api_queue:run"
-  end
-else 
-  every :wednesday, :at => '12am' do
-    rake "api_queue:run[20,s3]"
-  end
-end
-
-
-# # On staging read the data from the archive and cache it
