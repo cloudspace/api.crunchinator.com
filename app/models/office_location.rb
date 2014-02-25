158de0e5cfa835c2c63f7d8a0f41561aeefbbcac
Justin Ridgewell <justin@ridgewell.name>
2014-02-25 13:23:28 -0500

    Use String#[] with regex to grab match

--- app/models/office_location.rb
+++ app/models/office_location.rb
@@ -30,10 +30,11 @@ class OfficeLocation < ActiveRecord::Base
   after_create :geolocate
 
   def geolocate
-    if country_code == 'USA' && zip_code.present?
-      zip_matches = zip_code.match(/\d{5}/)
-      return true if zip_matches.blank?
-      zip_geo = ZipCodeGeo.find_by_zip_code(zip_matches[0])
+    if country_code == 'USA' && zip_code?
+      zip = zip_code[/\d{5}/]
+      return true unless zip
+
+      zip_geo = ZipCodeGeo.find_by_zip_code(zip)
       if zip_geo
         self.latitude = zip_geo.latitude
         self.longitude = zip_geo.longitude
